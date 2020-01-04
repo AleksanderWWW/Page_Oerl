7d0b23638eea0af2970cc6e20b6835c27d011e5f
AleksanderWWW <58885668+AleksanderWWW@users.noreply.github.com>
2020-01-04 16:51:04 +0100

    Added styling to input form

--- mail.php
+++ mail.php
@@ -2,7 +2,7 @@
 $name = $_POST['name'];
 $surname = $_POST['surname']
 $email = $_POST['email'];
-$recipient = "emailaddress@here.com"; //generic mail of the company here
+$recipient = "alwojnarowicz@gmial.com"; //generic mail of the company here
 $subject = "Contact Form";
 $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $mailheader) or die("Error!");
